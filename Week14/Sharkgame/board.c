c8cfdd67955ee172c880bd013025f31293a002b3
yeonjoo2002 <yeonj000616@sookmyung.ac.kr>
2024-12-06 21:04:14 +0900

    Week 14

---
+++ Week14/Sharkgame/board.c
@@ -0,0 +1,101 @@
+//
+//  board.c
+//  sharkGame
+//
+
+#include "board.h"
+
+// ----- EX. 3 : board ------------
+#define N_COINPOS       12
+#define MAX_COIN        4
+// ----- EX. 3 : board ------------
+// ----- EX. 5 : shark ------------
+#define MAX_SHARKSTEP   6
+#define SHARK_INITPOS   -4
+// ----- EX. 5 : shark ------------
+// ----- EX. 3 : board ------------
+static int board_coin[N_BOARD];
+static int board_status[N_BOARD]; //0 - OK, 1 - destroyed
+// ----- EX. 3 : board ------------
+
+
+static int shark_position;
+
+// ----- EX. 3 : board ------------
+void board_printBoardStatus(void)
+{
+    int i;
+    
+    printf("----------------------- BOARD STATUS -----------------------\n");
+    for (i=0;i<N_BOARD;i++)
+    {
+        printf("|");
+        if (board_status[i] == BOARDSTATUS_NOK)
+            printf("X");
+        else
+            printf("O");
+    }
+    printf("|\n");
+    printf("------------------------------------------------------------\n");
+}
+
+int board_initBoard(void)
+{
+    int i;
+    
+    //variable initialization
+    for (i=0;i<N_BOARD;i++)
+    {
+        board_status[i] = BOARDSTATUS_OK;
+        board_coin[i] = 0;
+    }
+    
+// ----- EX. 5 : shark ------------
+    shark_position = SHARK_INITPOS;
+// ----- EX. 5 : shark ------------
+
+    //coin allocation
+   
+    
+    return N_COINPOS;
+}
+// ----- EX. 3 : board ------------
+
+
+// ----- EX. 5 : shark ------------
+int board_stepShark(void)
+{
+    int step = rand() % MAX_SHARKSTEP + 1;
+    int new_position = shark_position + step;
+    int i;
+
+    for (i = shark_position + 1; i <= new_position && i < N_BOARD; i++) {
+        if (i >= 0 && i < N_BOARD) {
+            board_status[i] = BOARDSTATUS_NOK;
+        }
+    }
+
+    shark_position = new_position;
+    if (shark_position >= N_BOARD) {
+        shark_position = SHARK_INITPOS;
+    }
+
+    return shark_position;
+}
+
+// ----- EX. 5 : shark ------------
+
+
+// ----- EX. 3 : board ------------
+int board_getBoardStatus(int pos)
+{
+    return board_status[pos];
+}
+
+int board_getBoardCoin(int pos)
+{
+    int coin = board_coin[pos];
+    board_coin[pos] = 0;
+    return coin;
+}
+// ----- EX. 3 : board ------------
